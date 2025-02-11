e2b81480edffb2d1b5ca7450b32dbd8434a2eb69
Rob Randell <rob.randell@icloud.com>
2025-02-11 13:19:45 +0000

    refactor: more function to separate file

--- app/lib/map.tsx
+++ app/lib/map.tsx
@@ -3,28 +3,10 @@
 import { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import { Record, CollisionProperties } from '@/app/lib/definitions';
+import getRoadClassForNumber from '@/app/lib/utils';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
-
-const getRoadClassForNumber = (num: number, route: number): string | undefined => {
-  switch (num) {
-    case 1:
-      return '<p class="motorway inline-block mb-1 py-0 px-2 rounded-sm text-white">M' + route + '</p>';
-    case 2:
-      return '<p class="am-road inline-block mb-1 py-0 px-2 rounded-sm text-white">A(M)' + route + '</p>';
-    case 3:
-      return '<p class="a-road inline-block mb-1 py-0 px-2 rounded-sm">A' + route + '</p>';
-    case 4:
-      return '<p class="b-road inline-block mb-1 py-0 px-2 rounded-sm">B' + route + '</p>';
-    case 5:
-      return '<p class="c-road inline-block mb-1 py-0 px-2 rounded-sm">C' + route + '</p>';
-    case 6:
-      return '<p class="unclassified inline-block mb-1 py-0 px-2 rounded-sm text-white bg-black">Unclassified</p>';
-    default:
-      return '<p class="data-missing inline-block mb-1 py-0 px-2 rounded-sm text-white bg-black">Data missing or out of range</p>';
-  }
-};
 
 const Map = ({ onFeatureClick, mapData, loading } : { onFeatureClick: (id: string) => void, mapData: Record[] | null, loading: boolean }) => {
   const mapContainerRef = useRef<HTMLDivElement>(null);
