e1e9322d09184f18eed8180589f823603871ae0a
Rob Randell <rob.randell@icloud.com>
2025-02-14 11:57:38 +0000

    refactor: update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -85,5 +86,5 @@ export default {
       drawerSlideRightAndFade: "drawerSlideRightAndFade 150ms ease-in",
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 } satisfies Config;
